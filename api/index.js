57251416567d02cb35c1c737ace49c04361beee5
hoaaminzy <129186660+hoaaminzy@users.noreply.github.com>
2024-05-06 10:36:53 +0700

    add

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouters from "./routes/user.route.js";
 import authRouters from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from 'path'
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -15,7 +16,11 @@ mongoose
   })
   .catch((err) => console.log(err));
 // const PORT = process.env.PORT
-
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname, 'client', 'dist' , 'index.html'))
+})
 app.use("/api/user", userRouters);
 app.use("/api/auth", authRouters);
 app.use((err, req, res, next) => {
